16b710422df29c171ffdb8cd1cbe34204b9772ed
Chahat <144444393+chahatbashani2304@users.noreply.github.com>
2024-07-12 15:07:40 +0530

    Add files via upload

---
+++ frontend/src/Pages/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HeaderText from './Headertect'
+import Form from './Form'
+// import './CSS/Card.css'
+
+export default function Card() {
+   return (
+      <main>
+         <HeaderText />
+         <Form />
+    </main>
+
+   )
+}
